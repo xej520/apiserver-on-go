b0ac60ad40d86feec89ce32160577f73343e243a
邢尔军 <xingej@163.com>
2018-09-16 13:48:14 +0800

    记录和管理API日志

---
+++ demo03/router/router.go
@@ -0,0 +1,33 @@
+package router
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"xingej-go/Apiserver-go/demo03/router/middleware"
+	"xingej-go/Apiserver-go/demo03/handler/sd"
+)
+
+// Load loads the middlewares, routes, handlers.
+func Load(g *gin.Engine, mw ...gin.HandlerFunc) *gin.Engine {
+	// Middlewares.
+	g.Use(gin.Recovery())
+	g.Use(middleware.NoCache)
+	g.Use(middleware.Options)
+	g.Use(middleware.Secure)
+	g.Use(mw...)
+	// 404 Handler.
+	g.NoRoute(func(c *gin.Context) {
+		c.String(http.StatusNotFound, "The incorrect API route.")
+	})
+
+	// The health check handlers
+	svcd := g.Group("/sd")
+	{
+		svcd.GET("/health", sd.HealthCheck)
+		svcd.GET("/disk", sd.DiskCheck)
+		svcd.GET("/cpu", sd.CPUCheck)
+		svcd.GET("/ram", sd.RAMCheck)
+	}
+
+	return g
+}
